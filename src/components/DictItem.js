9e621d5c10654e90d6e10745b2c1e5ecad6c5a2b
zaxwebs <zifidigital@gmail.com>
2019-11-26 08:25:12 +0530

    Restrict results to top 20 to save nodes

--- src/components/DictItem.js
+++ src/components/DictItem.js
@@ -2,23 +2,23 @@ import React, { useContext } from 'react'
 import Context from '../Context'
 
 const DictItem = props => {
-  const { swapped } = useContext(Context)
-  const english = (
-    <>
-      <span className="english">{props.content.english}</span>
-      {props.content.POS && <span className="POS"> ({props.content.POS})</span>}
-    </>
-  )
-  const dhivehi = (
-    <div className="dhivehi">{props.content.dhivehi.join(', ')}</div>
-  )
-  return (
-    <div className="dict-item box">
-      <div className="top">{swapped ? dhivehi : english}</div>
-      <div className="divider" />
-      {swapped ? english : dhivehi}
-    </div>
-  )
+	const { swapped } = useContext(Context)
+	const english = (
+		<>
+			<span className="english">{props.content.english}</span>{' '}
+			{props.content.POS && <span className="POS">{props.content.POS}</span>}
+		</>
+	)
+	const dhivehi = (
+		<div className="dhivehi">{props.content.dhivehi.join(', ')}</div>
+	)
+	return (
+		<div className="dict-item box">
+			<div className="top">{swapped ? dhivehi : english}</div>
+			<div className="divider" />
+			{swapped ? english : dhivehi}
+		</div>
+	)
 }
 
 export default DictItem
--- src/components/SearchResults.js
+++ src/components/SearchResults.js
@@ -3,45 +3,47 @@ import DictList from './DictList'
 import Context from '../Context'
 
 const SearchResults = ({ term, dictionary }) => {
-  const { swapped } = useContext(Context)
+	const { swapped } = useContext(Context)
 
-  const getResults = () => {
-    return dictionary.filter(item => {
-      if (swapped) {
-        return item.dhivehi.some(element =>
-          element.toLowerCase().startsWith(term.toLowerCase())
-        )
-      } else {
-        return item.english.toLowerCase().startsWith(term.toLowerCase())
-      }
-    })
-  }
+	const getResults = () => {
+		return dictionary
+			.filter(item => {
+				if (swapped) {
+					return item.dhivehi.some(element =>
+						element.toLowerCase().startsWith(term.toLowerCase())
+					)
+				} else {
+					return item.english.toLowerCase().startsWith(term.toLowerCase())
+				}
+			})
+			.slice(0, 20) //return first 20 results, saves nodes
+	}
 
-  if (!term) {
-    return false
-  } else {
-    const results = getResults()
-    if (results[0]) {
-      return (
-        <>
-          <h3>Here's What We Found For You</h3>
-          <DictList items={results} />
-        </>
-      )
-    } else {
-      return (
-        <>
-          <h3>Oops, We Couldn't Find Anything</h3>
-          <div className="box">
-            <p>
-              Looks like we might not have any translation for that at the
-              moment.
-            </p>
-          </div>
-        </>
-      )
-    }
-  }
+	if (!term) {
+		return false
+	} else {
+		const results = getResults()
+		if (results[0]) {
+			return (
+				<>
+					<h3>Here's What We Found For You</h3>
+					<DictList items={results} />
+				</>
+			)
+		} else {
+			return (
+				<>
+					<h3>Oops, We Couldn't Find Anything</h3>
+					<div className="box">
+						<p>
+							Looks like we might not have any translation for that at the
+							moment.
+						</p>
+					</div>
+				</>
+			)
+		}
+	}
 }
 
 export default SearchResults
